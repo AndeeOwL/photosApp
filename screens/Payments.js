54acdd6da3b4d7e1b7b27eea6ffe9c76697f754d
andrean.buhchev <andeeowl@yahoo.com>
2022-12-05 19:01:34 +0200

    minor ui fixes

--- screens/Payments.js
+++ screens/Payments.js
@@ -9,7 +9,7 @@ import {
   presentApplePay,
 } from "@stripe/stripe-react-native";
 import { useEffect } from "react";
-import { Alert, Platform, View } from "react-native";
+import { Alert, Platform, StyleSheet, Text, View } from "react-native";
 import { subscribe } from "../util/database";
 
 function Payments({ route }) {
@@ -110,29 +110,13 @@ function Payments({ route }) {
 
   return (
     <>
-      <CardField
-        postalCodeEnabled={true}
-        placeholders={{
-          number: "4242 4242 4242 4242",
-        }}
-        cardStyle={{
-          backgroundColor: "#FFFFFF",
-          textColor: "#000000",
-        }}
-        style={{
-          width: "100%",
-          height: 50,
-          marginVertical: 30,
-        }}
-        onCardChange={(cardDetails) => {
-          console.log("cardDetails", cardDetails);
-        }}
-        onFocus={(focusedField) => {
-          console.log("focusField", focusedField);
-        }}
-      />
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          Here you can buy your subscription for 5$
+        </Text>
+      </View>
       {Platform.OS === "android" && (
-        <View>
+        <View style={styles.payButton}>
           <GooglePayButton
             type='standard'
             onPress={createPaymentMethod}
@@ -144,7 +128,7 @@ function Payments({ route }) {
         </View>
       )}
       {Platform.OS === "ios" && (
-        <View>
+        <View style={styles.payButton}>
           {isApplePaySupported && (
             <ApplePayButton
               onPress={pay}
@@ -164,3 +148,17 @@ function Payments({ route }) {
 }
 
 export default Payments;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 32,
+  },
+  payButton: {
+    marginBottom: 50,
+  },
+});
